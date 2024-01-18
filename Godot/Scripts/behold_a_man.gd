9ffe4509058e26ee527106c4445b06b05372ceaf
s011d523k <s011d523k@outlook.com>
2024-01-17 22:18:28 -0500

    Add the ability to roll
    player can now press shift and roll, player moves faster while rolling than running

--- Godot/Scripts/behold_a_man.gd
+++ Godot/Scripts/behold_a_man.gd
@@ -4,15 +4,22 @@ extends CharacterBody3D
 @onready var spring_arm = $SpringArmPivot/SpringArm3D
 @onready var armature = $Armature
 @onready var anim_tree = $AnimationTree
+@onready var anim_player = $AnimationPlayer
 
-const SPEED = 5.0
 const LERP_VAL = .15
+
+const RUN_SPEED = 5.0
+const ROLL_SPEED = 10.0
+
+var is_rolling = false
+var speed = RUN_SPEED
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	anim_tree.active = true
 
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("quit"):
@@ -23,11 +30,18 @@ func _unhandled_input(event):
 		spring_arm.rotate_x(-event.relative.y * 0.005)
 		spring_arm.rotation.x = clamp(spring_arm.rotation.x, -PI/4, PI/4)
 
+func _process(delta):
+	update_animation_parameters()
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+		
+	if (is_rolling):
+		speed = ROLL_SPEED
+	else:
+		speed = RUN_SPEED
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -35,13 +49,27 @@ func _physics_process(delta):
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	direction = direction.rotated(Vector3.UP, spring_arm_pivot.rotation.y)
 	if direction:
-		velocity.x = lerp(velocity.x, direction.x * SPEED, LERP_VAL)
-		velocity.z = lerp(velocity.z, direction.z * SPEED, LERP_VAL)
+		velocity.x = lerp(velocity.x, direction.x * speed, LERP_VAL)
+		velocity.z = lerp(velocity.z, direction.z * speed, LERP_VAL)
 		armature.rotation.y = lerp_angle(armature.rotation.y, atan2(-velocity.x, -velocity.z), LERP_VAL)
 	else:
 		velocity.x = lerp(velocity.x, 0.0, LERP_VAL)
 		velocity.z = lerp(velocity.z, 0.0, LERP_VAL)
 		
-	anim_tree.set("parameters/BlendSpace1D/blend_position", velocity.length() / SPEED)
+	anim_tree.set("parameters/BlendSpace1D/blend_position", velocity.length() / speed)
 
 	move_and_slide()
+
+func update_animation_parameters():
+	var is_moving = velocity != Vector3.ZERO
+	
+	if (is_moving && Input.is_action_just_pressed("roll")):
+		anim_tree["parameters/conditions/is_rolling"] = true
+		is_rolling = true
+	else:
+		anim_tree["parameters/conditions/is_rolling"] = false
+
+
+func _on_animation_tree_animation_finished(anim_name):
+	if (anim_name == "roll"):
+		is_rolling = false
--- tutorials_used.md
+++ tutorials_used.md
@@ -1,12 +1,32 @@
 # Tutorials Used
 
-## Basics of Godot and Blender Animation:
+## Basics of Godot and Blender Animation
 
-Tutorial Video: [Godot 4 / Blender - Third Person Character From Scratch](https://www.youtube.com/watch?v=VasHZZyPpYU)
+### Tutorial Video
 
-Contents and timestamps:
+[Godot 4 / Blender - Third Person Character From Scratch](https://www.youtube.com/watch?v=VasHZZyPpYU)
+
+### Why watch it
+
+Learn how to create a controllable 3D character
+
+### Contents and timestamps
 
 - 00:53 - 19:30 : Blender modelling
 - 19:31 - 30:30 : Blender rigging
 - 30:31 - 44:05 : Blender animating
 - 44:05 - end : Godot setup
+
+## Animation State Machines
+
+### Tutorial Video
+
+[Animation Tree State Machine Setup w/ Conditions & BlendSpace2D - Godot 4 Resource Gatherer Tutorial](https://www.youtube.com/watch?v=WrMORzl3g1U)
+
+### Why watch it
+
+Learn how to setup a animation state machine to control the player model while the player performs different actions. For example, add a roll animation that triggers when the player presses space, and transition back and forth to a blendspace1D for standing still / running.
+
+### Contents and timestamps
+
+- 00:00 - 07:00ish : setup the state machine, setup transition parameters, set transition parameters in code
